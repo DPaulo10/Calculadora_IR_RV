0c0259a331ed238de5623a0ff7384a8335e52642
DPaulo10 <142446794+DPaulo10@users.noreply.github.com>
2023-08-30 16:12:04 -0300

    Add files via upload

--- Calculadora_IR.v4.py
+++ Calculadora_IR.v4.py
@@ -4,16 +4,36 @@ while True:
         while True:
             tipo_operacao = input('Qual o tipo de operação (Day trade/Normal)?').title().strip()
             if tipo_operacao == 'Day Trade':
-                preco_compra = float(input('Qual o preço da compra?'))
-                preco_venda = float(input('Qual o preço da venda?'))
-                quantidade = int(input('Qual a quantidade?'))
+                while True:
+                    try:
+                        preco_compra = float(input('Qual o preço da compra?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
+                while True:
+                    try:
+                        preco_venda = float(input('Qual o preço da venda?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
+                while True:
+                    try:
+                        quantidade = int(input('Qual a quantidade?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
                 resultado = ((preco_venda - preco_compra) * quantidade)
                 if resultado * 0.2 > 10:
-                    credito_tributario = float(input('Se você possui créditos tributários provenientes de '
+                    while True:
+                        try:
+                            credito_tributario = float(input('Se você possui créditos tributários provenientes de '
                                                      'operações do tipo Day Trade e ainda não os utilizou '
                                                      'para abatimento, por favor, informe o valor aqui. '
                                                      '(Caso não possua créditos tributários, digite o valor '
                                                      '0.00).'))
+                            break
+                        except:
+                            print('Valor inválido. Digite um número.')
                     print(f'O imposto de renda a recolher é de R$ {(resultado - credito_tributario) * 0.2}')
                     break
                 elif resultado < 0:
@@ -25,16 +45,36 @@ while True:
                           f'futuro apenas quando a soma do Imposto de Renda (IR) for superior a R$ 10,00.')
                     break
             elif tipo_operacao == 'Normal':
-                preco_compra = float(input('Qual o preço da compra?'))
-                preco_venda = float(input('Qual o preço da venda?'))
-                quantidade = int(input('Qual a quantidade da venda?'))
+                while True:
+                    try:
+                        preco_compra = float(input('Qual o preço da compra?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
+                while True:
+                    try:
+                        preco_venda = float(input('Qual o preço da venda?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
+                while True:
+                    try:
+                        quantidade = int(input('Qual a quantidade da venda?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
                 total_venda = quantidade * preco_venda
                 resultado = ((preco_venda - preco_compra) * quantidade)
                 if total_venda > 20000 and resultado * 0.15 > 10.0:
-                    credito_tributario = float(input('Se você possui créditos tributários provenientes de operações'
+                    while True:
+                        try:
+                            credito_tributario = float(input('Se você possui créditos tributários provenientes de operações'
                                                      ' do tipo Day Trade e ainda não os utilizou para abatimento, '
                                                      'por favor, informe o valor aqui. (Caso não possua créditos '
                                                      'tributários, digite o valor 0.00).'))
+                            break
+                        except:
+                            print('Valor inválido. Digite um número.')
                     print(f'O imposto de renda a recolher é de R$ {(resultado - credito_tributario) * 0.15}')
                     break
                 elif resultado < 0:
@@ -55,16 +95,36 @@ while True:
         while True:
             tipo_operacao = input('Qual o tipo de operação (Day trade/Normal)?').title().strip()
             if tipo_operacao == 'Day Trade':
-                preco_venda = float(input('Qual o preço da venda?'))
-                preco_compra = float(input('Qual o preço da compra?'))
-                quantidade = int(input('Qual a quantidade da venda?'))
+                while True:
+                    try:
+                        preco_venda = float(input('Qual o preço da venda?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
+                while True:
+                    try:
+                        preco_compra = float(input('Qual o preço da compra?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
+                while True:
+                    try:
+                        quantidade = int(input('Qual a quantidade da venda?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
                 resultado = ((preco_venda - preco_compra) * quantidade)
                 if resultado * 0.2 > 10:
-                    credito_tributario = float(input('Se você possui créditos tributários provenientes de '
-                                                     'operações do tipo Day Trade e ainda não os utilizou '
-                                                     'para abatimento, por favor, informe o valor aqui. '
-                                                    '(Caso não possua créditos tributários, digite o valor '
-                                                     '0.00).'))
+                    while True:
+                        try:
+                            credito_tributario = float(input('Se você possui créditos tributários provenientes de '
+                                                             'operações do tipo Day Trade e ainda não os utilizou '
+                                                             'para abatimento, por favor, informe o valor aqui. '
+                                                             '(Caso não possua créditos tributários, digite o valor '
+                                                             '0.00).'))
+                            break
+                        except:
+                            print('Valor inválido. Digite um número.')
                     print(f'O imposto de renda a recolher é de R$ {(resultado - credito_tributario) * 0.2}')
                     break
                 elif resultado < 0:
@@ -76,18 +136,37 @@ while True:
                           f'futuro apenas quando a soma do Imposto de Renda (IR) for superior a R$ 10,00.')
                     break
             elif tipo_operacao == 'Normal':
-                preco_venda = float(input('Qual o preço da venda?'))
-                preco_compra = float(input('Qual o preço da compra?'))
-                quantidade = int(input('Qual a quantidade da venda?'))
+                while True:
+                    try:
+                        preco_venda = float(input('Qual o preço da venda?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
+                while True:
+                    try:
+                        preco_compra = float(input('Qual o preço da compra?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
+                while True:
+                    try:
+                        quantidade = int(input('Qual a quantidade da venda?'))
+                        break
+                    except:
+                        print('Valor inválido. Digite um número.')
                 total_venda = quantidade * preco_venda
                 resultado = ((preco_venda - preco_compra) * quantidade)
                 if total_venda > 20000 and resultado * 0.15 > 10.0:
-                    credito_tributario = float(input('Se você possui créditos tributários provenientes de operações'
-                                                     ' do tipo Day Trade e ainda não os utilizou para abatimento, '
-                                                     'por favor, informe o valor aqui. (Caso não possua créditos '
-                                                     'tributários, digite o valor 0.00).'))
-                    print(f'O imposto de renda a recolher é de R$ {(resultado - credito_tributario) * 0.15}')
-                    break
+                    while True:
+                        try:
+                            credito_tributario = float(input('Se você possui créditos tributários provenientes de '
+                                                             'operações do tipo Day Trade e ainda não os utilizou '
+                                                             'para abatimento, por favor, informe o valor aqui. '
+                                                             '(Caso não possua créditos tributários, digite o valor '
+                                                             '0.00).'))
+                            break
+                        except:
+                            print('Valor inválido. Digite um número.')
                 elif resultado < 0:
                     print(f'Isento. Essa operação gerou um crédito tributário de R$ {abs(resultado)}.')
                     break
@@ -104,4 +183,3 @@ while True:
         break
     else:
         print('Informe se a operação foi de compra ou de venda.')
-
